16152da6c0a8e65343304be06bf1421fc74dfa37
Atoilah <atoilahputra016@gmail.com>
2022-12-26 08:27:49 +0700

    project dekstop

---
+++ desktop/TuttiFruitti/GUI/Login.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace TuttiFruitti.GUI
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.roundButton1 = new TuttiFruitti.Class.RoundButton();
+            this.round2 = new TuttiFruitti.Class.round();
+            this.round1 = new TuttiFruitti.Class.round();
+            this.SuspendLayout();
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label6.Font = new System.Drawing.Font("Poppins", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(89)))), ((int)(((byte)(1)))), ((int)(((byte)(255)))));
+            this.label6.Location = new System.Drawing.Point(637, 375);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(141, 28);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "Buat Sekarang ?";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Poppins", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(47)))), ((int)(((byte)(114)))));
+            this.label5.Location = new System.Drawing.Point(461, 375);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(170, 28);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Belum Punya Akun ?";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label4.Font = new System.Drawing.Font("Poppins", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(47)))), ((int)(((byte)(114)))));
+            this.label4.Location = new System.Drawing.Point(329, 167);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(143, 28);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Lupa Password ?";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Poppins Medium", 10F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(47)))), ((int)(((byte)(155)))));
+            this.label3.Location = new System.Drawing.Point(104, 220);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 36);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Password";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Poppins Medium", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(47)))), ((int)(((byte)(155)))));
+            this.label2.Location = new System.Drawing.Point(104, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 36);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Email";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Poppins", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(47)))), ((int)(((byte)(155)))));
+            this.label1.Location = new System.Drawing.Point(116, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(362, 56);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Masuk Ke TuttiFruitti";
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(879, -2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(39, 35);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // roundButton1
+            // 
+            this.roundButton1.BackColor = System.Drawing.Color.Transparent;
+            this.roundButton1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(115)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.roundButton1.ButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(89)))), ((int)(((byte)(1)))), ((int)(((byte)(255)))));
+            this.roundButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.roundButton1.Font = new System.Drawing.Font("Poppins", 14F, System.Drawing.FontStyle.Bold);
+            this.roundButton1.ForeColor = System.Drawing.Color.White;
+            this.roundButton1.Location = new System.Drawing.Point(60, 318);
+            this.roundButton1.Name = "roundButton1";
+            this.roundButton1.OnHoverBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(89)))), ((int)(((byte)(1)))), ((int)(((byte)(255)))));
+            this.roundButton1.OnHoverButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(47)))), ((int)(((byte)(114)))));
+            this.roundButton1.OnHoverTextColor = System.Drawing.Color.White;
+            this.roundButton1.Size = new System.Drawing.Size(377, 64);
+            this.roundButton1.TabIndex = 24;
+            this.roundButton1.Text = "Masuk";
+            this.roundButton1.TextColor = System.Drawing.Color.White;
+            this.roundButton1.UseVisualStyleBackColor = false;
+            this.roundButton1.Click += new System.EventHandler(this.roundButton1_Click);
+            // 
+            // round2
+            // 
+            this.round2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.round2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.round2.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.round2.Location = new System.Drawing.Point(517, 255);
+            this.round2.Name = "round2";
+            this.round2.Size = new System.Drawing.Size(248, 36);
+            this.round2.TabIndex = 23;
+            this.round2.Text = " ";
+            // 
+            // round1
+            // 
+            this.round1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.round1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.round1.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.round1.Location = new System.Drawing.Point(517, 159);
+            this.round1.Name = "round1";
+            this.round1.Size = new System.Drawing.Size(248, 36);
+            this.round1.TabIndex = 22;
+            this.round1.Text = " ";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(917, 460);
+            this.Controls.Add(this.roundButton1);
+            this.Controls.Add(this.round2);
+            this.Controls.Add(this.round1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Class.RoundButton roundButton1;
+        private Class.round round2;
+        private Class.round round1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ desktop/TuttiFruitti/GUI/Login.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TuttiFruitti.GUI
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void roundButton1_Click(object sender, EventArgs e)
+        {
+            Menu mn = new Menu();
+            mn.Show();
+            Visible = false;
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://www.xhamster.com");
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://www.xnxx.com");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Dispose();
+        }
+    }
+}
